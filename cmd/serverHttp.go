b2028ff65a1555483c9f756572644d16e68f1c32
Kirill Semerenko <semerenkok@rambler.ru>
2022-03-03 10:32:42 +0300

    http completed

---
+++ cmd/serverHttp.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"30module/internal/httpReqests"
+	"github.com/go-chi/chi"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	nr := chi.NewRouter()
+	nr.MethodFunc("GET", "/users", httpRequests.HttpGetUsers)
+	nr.MethodFunc("POST", "/create", httpRequests.HttpCreateUser)
+	nr.MethodFunc("GET", "/friends/{id}", httpRequests.HttpGetUserFriends)
+	nr.MethodFunc("PUT", "/{id}", httpRequests.HttpUpdateUserAge)
+	nr.MethodFunc("DELETE", "/user", httpRequests.HttpDeleteUser)
+	nr.MethodFunc("POST", "/make_friends", httpRequests.HttpMakeFriends)
+
+	log.Fatal(http.ListenAndServe(":8080", nr))
+}
